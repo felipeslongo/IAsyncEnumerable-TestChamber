e8f6df762751a1baed7bfc74a5a240c502acb751
Felipe de Souza Longo <felipeslongo@gmail.com>
2020-09-18 16:05:49 -0300

    Add project files.

---
+++ IAsyncEnumerable.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using IAsyncEnumerable.ConsoleApp.RangeAsyncNS;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerable.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press 'Q' key to Quit!");
+            var cancellationTokenSource = new CancellationTokenSource();
+            var userQuitTask = AwaitUserQuitAsync();
+
+            var task = RangeAsync(cancellationTokenSource.Token);
+            
+            if (await Task.WhenAny(task, userQuitTask) == userQuitTask)
+                Console.WriteLine("User quit requested");
+            else
+                Console.WriteLine("Task Finished Successfully");
+
+            cancellationTokenSource.Cancel();
+            Console.WriteLine("Bye World!");
+        }
+
+        private static async Task RangeAsync(CancellationToken token = default)
+        {
+            await foreach (var item in new RangeAsync(1, 10).WithCancellation(token))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static async Task AwaitUserQuitAsync()
+        {
+            await Task.Run(() => 
+            {
+                while (Console.ReadKey().Key != ConsoleKey.Q) ;
+            });
+        }
+    }
+}
---
+++ IAsyncEnumerable.ConsoleApp/RangeAsyncNS/RangeAsync.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IAsyncEnumerable.ConsoleApp.RangeAsyncNS
+{
+    /// <summary>
+    ///     Generates a sequence of integral numbers within a specified range.
+    /// </summary>
+    public sealed class RangeAsync : IAsyncEnumerable<int>
+    {
+        public RangeAsync(int start, int count)
+        {
+            Start = start;
+            Count = count;
+        }
+
+        /// <summary>
+        ///     The value of the first integer in the sequence.
+        /// </summary>
+        public int Start { get; }
+        /// <summary>
+        ///     The number of sequential integers to generate.
+        /// </summary>
+        public int Count { get; }
+
+        public async IAsyncEnumerator<int> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in Enumerable.Range(Start, Count))
+            {
+                await Task.Delay(1000, cancellationToken);
+                cancellationToken.ThrowIfCancellationRequested();
+                yield return item;
+            }
+        }
+    }
+}
